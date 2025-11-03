b415e9ff90e19b367e37e8eaf2d886fd0758d02d
Vivek Singh <viveksingh1842004@gmail.com>
2025-11-03 13:00:08 +0530

    delete check file

--- check.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyAgFhu7_ZSUS0fFt3MxugteJj2NsoLQPwM")
-
-for m in genai.list_models():
-    if "generateContent" in m.supported_generation_methods:
-        print(m.name)
